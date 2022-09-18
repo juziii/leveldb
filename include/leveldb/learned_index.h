89fb2dad0022e509b23b0f3c6fd751740fc3ad8d
Administrator <790989228@QQ.com>
2022-09-18 21:09:25 +0800

    learned_index start!

---
+++ include/leveldb/learned_index.h
@@ -0,0 +1,12 @@
+//
+// Created by Administrator on 2022/9/18.
+//
+
+#ifndef LEVELDB_LEARNED_INDEX_H
+#define LEVELDB_LEARNED_INDEX_H
+namespace leveldb {
+class LearnedIndex{
+
+};
+}
+#endif  // LEVELDB_LEARNED_INDEX_H
